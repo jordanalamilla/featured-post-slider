061b54d08b8782b7606d324626c8d7a242aa38fc
Jordan Michael Alamilla <jordan.alamilla@gmail.com>
2026-05-12 10:09:43 -0400

    initial commit - installed wordpress create-block scaffolding

---
+++ build/featured-post-slider/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '1564446e7cdb8758f0c2');
---
+++ build/featured-post-slider/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'b395afc5add740b9e35e');
